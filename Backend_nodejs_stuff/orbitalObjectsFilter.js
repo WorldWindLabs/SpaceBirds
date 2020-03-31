ddf3c6067e2985311ae38215fc1404a5fd234585
Miguel Del Castillo <migueldelcastillo@live.com>
2020-03-31 13:21:16 -0500

    Updated data and data acquisition script. Added documentation

--- Backend_nodejs_stuff/orbitalObjectsFilter.js
+++ Backend_nodejs_stuff/orbitalObjectsFilter.js
@@ -1,14 +1,14 @@
+// This script processes a JSON file obtained from the 
+
 var fs = require('fs'); //IO filesystem module for node.js
-var orbitalBodies = require('./TLE.json'); //Unfiltered TLE JSON file to read
+var orbitalBodies = require('./PruebaTLE.json'); //Unfiltered TLE JSON file to read
 var payloads = [];
 var rocketStages = [];
 var debris = [];
-var unknown = [];
+var unclassified = [];
 var everythingButDebris = [];
 
 
-//Crap and unnecesary OOP implementation of a cleaning function
-//wastes a lot of memory/CPU but it keeps the main loop easy to understand.
 for(i in orbitalBodies){
   orbitalBodies[i].clean = function(){
     delete this.ORDINAL;
@@ -42,8 +42,9 @@ for(i in orbitalBodies){
   }
 }
 
-function printFilteredTLE(tleJson, type){
-  var str; //To stringify JSON arrays
+// Generic function to stringify JSON arrays and store them into a file.
+function storeFilteredTLE(tleJson, type){
+  var str; 
   str = JSON.stringify(tleJson);
   if(tleJson.length > 0){
     fs.writeFile(type + 'TLE.json', str,
@@ -57,6 +58,19 @@ function printFilteredTLE(tleJson, type){
   }
 }
 
+// Review if there are bodies of types other than payloads, rocket bodies, and generic debris.
+// Some objects are awaiting classification and ar marked as type TBA ("To Be Announced").
+for(var i = 1, unclassifiedObjectCount = 0 ; i < orbitalBodies.length; i += 1){
+  if((orbitalBodies[i].OBJECT_TYPE !== "PAYLOAD") &&
+     (orbitalBodies[i].OBJECT_TYPE !== "ROCKET BODY") &&
+     (orbitalBodies[i].OBJECT_TYPE !== "DEBRIS") &&
+     (orbitalBodies[i].OBJECT_TYPE !== "TBA")) {
+    unclassifiedObjectCount += 1;
+  }
+  console.log("Encountered object of type" + orbitalBodies[i].OBJECT_TYPE);
+}
+console.log("Encountered " + unclassifiedObjectCount + " unclassified objects.");
+
 for(i in orbitalBodies){
   switch(orbitalBodies[i].OBJECT_TYPE){
     case "PAYLOAD":
@@ -65,21 +79,15 @@ for(i in orbitalBodies){
       break;
     case "DEBRIS":
       break;
-    case default:
+    default:
       break;
   }
 }
 
-//Check out for other object types. It happens that there's a "TBA" type.
-for(var i = 1; i < orbitalBodies.length; i += 1){
-  if((orbitalBodies[i].OBJECT_TYPE !== "PAYLOAD") && (orbitalBodies[i].OBJECT_TYPE !== "ROCKET BODY") && (orbitalBodies[i].OBJECT_TYPE !== "DEBRIS") ){
-    console.log(orbitalBodies[i].OBJECT_TYPE);
-  }
-}
+// Split the TLE data obtained from Space track into different JSON arrays for each body type
+// for each option, the OBJECT_TYPE key is deleted before storing
+// it in its filtered array in order to reduce file sizes
 
-//Filter
-//for each option, the OBJECT_TYPE key is deleted before storing
-//it in its filtered array in order to reduce file sizes
 for(i in orbitalBodies){
   if(orbitalBodies[i].OBJECT_TYPE === "PAYLOAD" ){
     orbitalBodies[i].clean();
@@ -90,20 +98,23 @@ for(i in orbitalBodies){
   } else if (orbitalBodies[i].OBJECT_TYPE === "DEBRIS") {
     orbitalBodies[i].clean();
     debris.push(orbitalBodies[i]);
-  } else if (orbitalBodies[i].OBJECT_TYPE === "UNKNOWN"){
+  } else if (orbitalBodies[i].OBJECT_TYPE === "UNCLASSIFIED"){
     orbitalBodies[i].clean();
-    unknown.push(orbitalBodies[i]);
+    unclassified.push(orbitalBodies[i]);
   }
 }
 
+// Print summary of TLE contents
 console.log("From a total of " + orbitalBodies.length + " currently orbiting bodies we have: ");
 console.log(" - " + payloads.length + " payloads");
 console.log(" - " + rocketStages.length + " rocket stages");
 console.log(" - " + debris.length + " debris objects");
-console.log(" - " + unknown.length + " uknwown objects");
+console.log(" - " + toBeAnnounced.length + " to be announced objects")
+console.log(" - " + unclassified.length + " unclassified objects");
 
-printFilteredTLE(orbitalBodies, 'allObjects');
-printFilteredTLE(payloads, 'payloads');
-printFilteredTLE(rocketStages, 'rocketStages');
-printFilteredTLE(debris, 'debris');
-printFilteredTLE(unknown, 'unknownObjects');
+// Store separate JSON files for each type of object.
+storeFilteredTLE(orbitalBodies, 'allObjects');
+storeFilteredTLE(payloads, 'payloads');
+storeFilteredTLE(rocketStages, 'rocketStages');
+storeFilteredTLE(debris, 'debris');
+storeFilteredTLE(unclassified, 'unclassifiedObjects');
---
+++ standalone/SpaceBirdsRewrite.js
@@ -0,0 +1,312 @@
+"use strict";
+var allOrbitingBodies = []; //Global array with all the orbiting objects
+//Event handling to avoid redraw on mousedown to simulate stuttering elimination
+var satUpdateTimer = -1; //Global ID of mouse up interval. Note that mouse button IDs are 0,1,2...
+var loopTime = 5000; //Default value to update satellites position. It will get optimized in renderEverything()
+
+addEventListener("mousedown", mouseDown);
+addEventListener("mouseup", mouseUp);
+// addEventListener("mouseout", mouseup);
+addEventListener("onwheel", onWheel);
+
+WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+
+// Create the World Window.
+var wwd = new WorldWind.WorldWindow("canvasOne");
+//wwd.navigator.lookAtLocation.altitude = 0;
+wwd.navigator.range = 2e7;
+wwd.globe.elevationModel = new WorldWind.ZeroElevationModel();
+
+var FixedLocation = function(wwd) {
+    this._wwd = wwd;
+};
+
+FixedLocation.prototype = Object.create(WorldWind.Location.prototype);
+
+
+
+//Add imagery layers.
+var layers = [
+  {layer: new WorldWind.BMNGOneImageLayer(), enabled: true},
+  {layer: new WorldWind.BMNGLayer(), enabled: true},
+  {layer: new WorldWind.AtmosphereLayer(), enabled: true},
+  {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
+  {layer: new WorldWind.ViewControlsLayer(wwd), enabled: false}
+];
+
+for (var l = 0; l < layers.length; l++) {
+  layers[l].layer.enabled = layers[l].enabled;
+  wwd.addLayer(layers[l].layer);
+}
+
+layers[1].layer.maxActiveAltitude = 15e6;
+layers[2].layer.maxActiveAltitude = 15e7;
+layers[2].layer.lightLocation = new FixedLocation(wwd);
+
+//custom layers
+var groundStationsLayer = new WorldWind.RenderableLayer();
+//var modelLayer = new WorldWind.RenderableLayer("Model");
+//var meshLayer = new WorldWind.RenderableLayer();
+//var orbitsLayer = new WorldWind.RenderableLayer("Orbit");
+var payloadLayer = new WorldWind.RenderableLayer("Payloads");
+var rocketLayer = new WorldWind.RenderableLayer("Rocket bodies");
+var debrisLayer = new WorldWind.RenderableLayer("Debris");
+var selectedSatsLayer = new WorldWind.RenderableLayer("Selected satellites");
+
+//Abstraction of an orbital body
+function orbitalBody(satelliteData){
+  this.objectName = satelliteData.OBJECT_NAME;
+  this.tleLine1 = satelliteData.TLE_LINE1;
+  this.tleLine2 = satelliteData.TLE_LINE2;
+  this.intlDes = satelliteData.INTLDES;
+  this.objectType = satelliteData.OBJECT_TYPE;
+  this.orbitalPeriod = satelliteData.PERIOD;
+  this.latitude = null;
+  this.longitude = null;
+  this.altitude = null;
+  this.collada3dModel = retrieve3dModelPath(satelliteData.INTLDES);
+  this.orbitType = obtainOrbitType(satelliteData);
+  this.url = null; //to be added to satellite data. Patrick will provide URLs.
+}
+
+function deg2text(deg, letters) {
+  var letter;
+  if (deg < 0) {
+      letter = letters[1]
+  } else {
+      letter = letters[0]
+  }
+  var position = Math.abs(deg);
+  var degrees = Math.floor(position);
+  position -= degrees;
+  position *= 60;
+  var minutes = Math.floor(position);
+  position -= minutes;
+  position *= 60;
+  var seconds = Math.floor(position * 100) / 100;
+  return degrees + "° " + minutes + "' " + seconds + "\" " + letter;
+}
+
+// Orbit Propagation (MIT License, see https://github.com/shashwatak/satellite-js)
+function getPosition (satrec, time) {
+  var position_and_velocity = satellite.propagate(satrec,
+    time.getUTCFullYear(),
+    time.getUTCMonth() + 1,
+    time.getUTCDate(),
+    time.getUTCHours(),
+    time.getUTCMinutes(),
+    time.getUTCSeconds());
+  var position_eci = position_and_velocity["position"];
+
+  var gmst = satellite.gstime_from_date(time.getUTCFullYear(),
+    time.getUTCMonth() + 1,
+    time.getUTCDate(),
+    time.getUTCHours(),
+    time.getUTCMinutes(),
+    time.getUTCSeconds());
+
+  var position_gd = satellite.eci_to_geodetic(position_eci, gmst);
+  var latitude = satellite.degrees_lat(position_gd["latitude"]);
+  var longitude = satellite.degrees_long(position_gd["longitude"]);
+  var altitude = position_gd["height"] * 1000;
+
+  return new WorldWind.Position(latitude, longitude, altitude);
+};
+
+function getSatellites(satData){
+  var faultySatsNumber = 0;
+  var orbitalBodiesNumber = 0;
+  for(var i = 0; i < satData.length; i += 1){
+    try{
+      var position = getPosition(satellite.twoline2satrec(satData[i].TLE_LINE1, satData[i].TLE_LINE2), new Date());
+    } catch (err) {
+      faultySatsNumber += 1;
+      continue;
+    }
+    var myOrbitalBody = new orbitalBody(satData[i]);
+    myOrbitalBody.latitude = position.latitude;
+    myOrbitalBody.longitude = position.longitude;
+    myOrbitalBody.altitude = position.altitude;
+    orbitalBodiesNumber += 1;
+
+    switch (myOrbitalBody.objectType){
+      case "PAYLOAD":
+        payloadLayer.addRenderable(generatePlacemark(myOrbitalBody));
+        allOrbitingBodies.push(myOrbitalBody);
+        break;
+      case "ROCKET BODY":
+        rocketLayer.addRenderable(generatePlacemark(myOrbitalBody));
+        allOrbitingBodies.push(myOrbitalBody);
+        break;
+      case "DEBRIS":
+        debrisLayer.addRenderable(generatePlacemark(myOrbitalBody));
+        allOrbitingBodies.push(myOrbitalBody);
+        break;
+      }
+  }
+  console.log('We have ' + orbitalBodiesNumber + ' orbiting bodies');
+  console.log(faultySatsNumber + ' satellites had errors in their TLE.');
+  renderEverything();
+}
+
+function generatePlacemark(orbitalBody){
+  var placemarkPosition = new WorldWind.Position(orbitalBody.latitude, orbitalBody.longitude, orbitalBody.altitude);
+  var placemark = new WorldWind.Placemark(placemarkPosition);
+  var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+
+  var highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
+  highlightAttributes.imageScale = 0.70;
+  highlightAttributes.imageSource = "assets/icons/dot-green.png";
+  placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
+
+  switch(orbitalBody.objectType) {
+    case "PAYLOAD":
+      placemarkAttributes.imageSource = "assets/icons/dot-red.png";
+      placemarkAttributes.imageScale = 0.60;
+      break;
+    case "ROCKET BODY":
+      placemarkAttributes.imageSource = "assets/icons/dot-blue.png";
+      placemarkAttributes.imageScale = 0.60;
+      break;
+    case "DEBRIS":
+      placemarkAttributes.imageSource = "assets/icons/dot-grey.png";
+      placemarkAttributes.imageScale = 0.60;
+      break;
+  }
+
+  placemark.attributes = placemarkAttributes;
+  placemark.highlightAttributes = highlightAttributes;
+
+  return placemark;
+}
+
+function renderEverything(){
+  wwd.addLayer(payloadLayer);
+  wwd.addLayer(rocketLayer);
+  wwd.addLayer(debrisLayer);
+
+  var start = performance.now();
+  updatePositions();
+  var end = performance.now();
+  loopTime = end - start;
+  console.log("Updating all satellites' positions took " + loopTime + " ms. " +
+      "Now it will be updated every " + loopTime * 3 + " ms.");
+  satelliteUpdating(updatePositions, satUpdateTimer, loopTime * 3);
+}
+
+function satelliteUpdating(callbackFunction, timerID, delay){
+  clearInterval(timerID);
+  satUpdateTimer = setInterval(callbackFunction, delay);
+}
+
+function retrieve3dModelPath(intlDes){
+  var modelPath = "nothing here yet";
+  return modelPath;
+}
+
+function obtainOrbitType(satOrbit){
+  var orbitType = "nothing here yet";
+  return orbitType;
+}
+
+var satParserWorker = new Worker("Workers/satelliteParseWorker.js");
+var grndStationsWorker = new Worker("Workers/groundStationsWorker.js");
+
+//Initiating JSON parsing workers
+satParserWorker.postMessage("work, satellite parser, work!");
+grndStationsWorker.postMessage("and you too, groundstations servant!");
+
+//Retrieval of JSON file data from worker threads. Also, closing such threads.
+satParserWorker.addEventListener('message', function(event){
+  var satData = event.data;
+  satParserWorker.postMessage('close');
+  getSatellites(satData);
+}, false);
+
+grndStationsWorker.addEventListener('message', function(event){
+  var groundStations = event.data;
+  grndStationsWorker.postMessage('close');
+}, false);
+
+
+
+function updatePositions(){
+
+  var payloadCounter = 0;
+  var rocketCounter = 0;
+  var debrisCounter = 0;
+
+  for (var i = 0; i < allOrbitingBodies.length; i += 1) {
+    var newPosition = getPosition(
+      satellite.twoline2satrec(
+        allOrbitingBodies[i].tleLine1,
+        allOrbitingBodies[i].tleLine2),
+      new Date());
+
+    switch (allOrbitingBodies[i].objectType) {
+      case "PAYLOAD":
+        payloadLayer.renderables[payloadCounter].position.latitude = newPosition.latitude;
+        payloadLayer.renderables[payloadCounter].position.longitude = newPosition.longitude;
+        payloadLayer.renderables[payloadCounter++].position.altitude = newPosition.altitude;
+        break;
+      case "ROCKET BODY":
+        rocketLayer.renderables[rocketCounter].position.latitude = newPosition.latitude;
+        rocketLayer.renderables[rocketCounter].position.longitude = newPosition.longitude;
+        rocketLayer.renderables[rocketCounter++].position.altitude = newPosition.altitude;
+        break;
+      case "DEBRIS":
+         debrisLayer.renderables[debrisCounter].position.latitude = newPosition.latitude;
+         debrisLayer.renderables[debrisCounter].position.longitude = newPosition.longitude;
+         debrisLayer.renderables[debrisCounter++].position.altitude = newPosition.altitude;
+         break;
+    }
+    //TODO: update positions in allOrbitingBodies
+}
+  wwd.redraw();
+}
+
+function mouseDown(event) {
+  clearInterval(satUpdateTimer);
+}
+
+function onWheel(event) {
+  clearInterval(satUpdateTimer);
+}
+
+function mouseUp(event) {
+  satelliteUpdating(updatePositions, satUpdateTimer, loopTime);
+}
+
+Object.defineProperties(FixedLocation.prototype, {
+
+    latitude: {
+        get: function () {
+            return WorldWind.Location.greatCircleLocation(
+                this._wwd.navigator.lookAtLocation,
+                -40,
+                1.2,
+                new WorldWind.Location()
+            ).latitude;
+        }
+    },
+
+    longitude: {
+        get: function () {
+            return WorldWind.Location.greatCircleLocation(
+                this._wwd.navigator.lookAtLocation,
+                -40,
+                1.2,
+                new WorldWind.Location()
+            ).longitude;
+        }
+    }
+
+});
+
+// function wheel(event) {
+//   clearInterval(satUpdateTimer);
+// }
+
+// $(document).ready(function() {
+//
+// });
